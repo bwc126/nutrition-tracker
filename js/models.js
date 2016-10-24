983b98b3996b66b8ef8f95691aa5aff0fb59f544
Brian Coe <bwc126@gmail.com>
2016-10-23 23:43:05 -0400

    chore: Remove obsolete code

--- js/models.js
+++ js/models.js
@@ -15,10 +15,4 @@ var searchTerm;
       cals: 200
     }
   });
-  // searchTerm is the model for each term the user puts into the search field, giving us a standardized way to store a history of the user's past search phrases. Not used for much other than in the Search collection, at the moment, which isn't even persisted.
-  searchTerm = Backbone.Model.extend({
-    defaults: {
-      term: ''
-    }
-  });
 })(jQuery);
