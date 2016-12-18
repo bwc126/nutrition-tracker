6d84f63704b44786bfed0c2191708d7ce1fc820a
Brian Coe <bwc126@gmail.com>
2016-12-17 22:41:52 -0500

    fix: Changing calendar date causes userStorage retrieval

--- js/views.js
+++ js/views.js
@@ -56,6 +56,7 @@ var JumbotronView;
       self.activeDate = activeDate.format("MM-DD-YYYY");
       if (userStorage) {
         userStorage.date = self.activeDate;
+        userStorage.retrieve();
       }
     }
   });
